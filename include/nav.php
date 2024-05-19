a0bf43d7b8735f46712bb39da8b6c2d25852f796
David9249 <145716432+David9249@users.noreply.github.com>
2024-05-19 17:10:21 +0200

    first commit

---
+++ include/nav.php
@@ -0,0 +1,35 @@
+
+
+<link rel="stylesheet" href="../include/stylephp.css" >
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <img class="foto" src="../IMAGES/logo.png"></img>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <!-- <a class="nav-link " aria-current="page" href="HomePage.html">Home</a> -->
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost/RekenWebwite/sudoku/index.php">Sudoku</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost/RekenWebwite/tafelsoefenen/index.php">Tafels Oefenen</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost/RekenWebwite/Tafels/index.php">Tafels</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost/RekenWebwite/Som_Som_Magie/index.php" >Som-Som Magie</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+        
+        
+      </nav>
+      
+     
