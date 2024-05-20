5fbec32297f848d4e097340d5ad2befd8b5a2eb3
David9249 <145716432+David9249@users.noreply.github.com>
2024-05-21 00:05:35 +0200

    fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../HomePagina/style.css">
+    <link rel="stylesheet" href="HomePagina/style.css">
     
     <title>Mainpage</title>
 </head>
@@ -12,14 +12,14 @@
 
 <?php   
     // include '../include/nav.php';
-     include '../include/nav.php';
+     include 'include/nav.php';
     ?>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">  </script>
 
 
 
       
-      <img class="PIC" src="../IMAGES/codepic.jpg"></img>
+      <img class="PIC" src="IMAGES/codepic.jpg"></img>
       <H1>Math Apps</H1>
 </body>
 
