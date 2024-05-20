f8feea37721399a24a86a9b46ba9e623654a3461
David9249 <145716432+David9249@users.noreply.github.com>
2024-05-20 23:57:34 +0200

    fix

--- include/nav.php
+++ include/nav.php
@@ -16,13 +16,13 @@
                 <a class="nav-link" href="../tafelsoefenen/index.php">Tafels Oefenen</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="../Tafels/index.php">Tafels</a>
+                <a class="nav-link" href="../tafels/index.php">Tafels</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="../Som_Som_Magie/index.php" >Som-Som Magie</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="../sudoku/index.php">Sudoku</a>
+                <a class="nav-link" href="../Sudoku/index.php">Sudoku</a>
               </li>
             </ul>
           </div>
